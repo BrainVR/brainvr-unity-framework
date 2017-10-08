325c93d3401c648092b49bc820c02917229bbd84
hejtmy <hejtmy@gmail.com>
2017-10-08 05:47:33 -0700

    Updates navigation manager

---
+++ Navigation/Scripts/ArrowNavigation.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+namespace Assets.ExperimentAssets.Navigation
+{
+    public class ArrowNavigation : NavigationController
+    {
+        public GameObject Arrow;
+        public new const string Name = "Arrow";
+        public float Speed = 5f;
+        public Vector3 PlayerOffset = new Vector3(0, -0.1f, 0.1f);
+
+        void OnEnable()
+        {
+            if (!Arrow) Arrow = transform.GetChild(0).gameObject;
+        }
+        public override void OnNavigate()
+        {
+            PlayerHook();
+            Arrow.SetActive(true);
+        }
+        public override void OnStop()
+        {
+            Arrow.SetActive(false);
+            PlayerUnhook();
+        }
+        public override void OnUpdate()
+        {
+            RotateArrow();
+        }
+        private void PlayerHook()
+        {
+            Arrow.transform.SetParent(Camera.main.transform, false);
+            Arrow.transform.localPosition = PlayerOffset;
+        }
+        private void PlayerUnhook()
+        {
+            Arrow.transform.SetParent(transform, false);
+            Arrow.transform.SetPositionAndRotation(default(Vector3), default(Quaternion));
+        }
+        private void RotateArrow()
+        {
+            var difference = Target.position - Arrow.transform.position;
+            difference.y = 0F;     // Flatten the vector, assuming you're not concerned with indicating height difference
+            Arrow.transform.rotation = Quaternion.Slerp(Arrow.transform.rotation, Quaternion.LookRotation(difference.normalized), Time.deltaTime * Speed);
+        }
+        private void RotateArrow2()
+        {
+            Arrow.transform.LookAt(Target);
+        }
+    }
+}
---
+++ Navigation/Scripts/NavigationController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Assets.ExperimentAssets.Navigation
+{
+    public enum NavigatingState
+    {
+        Stopped,
+        Navigating
+    }
+
+    public abstract class NavigationController : MonoBehaviour
+    {
+        public NavigatingState State;
+        public Transform Target { get; set; }
+        public const string Name = "Default";
+
+        #region MonoBehaviour
+        void Update()
+        {
+            if (State == NavigatingState.Navigating) OnUpdate();
+        }
+        #endregion
+        #region Public API
+        #region Abstracts
+        public abstract void OnNavigate();
+        public abstract void OnStop();
+        public abstract void OnUpdate();
+        #endregion
+        #region Implemented
+        public void NavigateTo(GameObject obj)
+        {
+            Target = obj.transform;
+            Navigate();
+        }
+        public void Navigate()
+        {
+            State = NavigatingState.Navigating;
+            OnNavigate();
+        }
+        public void StopNavigation()
+        {
+            State = NavigatingState.Stopped;
+            OnStop();
+        }
+        #endregion
+
+        #endregion
+
+    }
+}
