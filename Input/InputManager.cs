0d3629cf41bb3e9ab4bde1d2baf3dacded3bbe7d
hejtmy <hejtmy@gmail.com>
2017-10-12 17:15:29 -0700

    Updates input manager to allow raising events from elsewhere. closes #23

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -6,11 +6,9 @@ namespace BrainVR.UnityFramework.InputControl
     public class InputManager : MonoBehaviour
     {
         public delegate void ButtonPressedHandler(string name);
-
         public static event ButtonPressedHandler ButtonPressed;
 
         public delegate void SpecialButtonHandler();
-
         public static event SpecialButtonHandler PointButtonPressed;
         public static event SpecialButtonHandler ConfirmationButtonPressed;
         public static event SpecialButtonHandler CancelButtonPressed;
@@ -19,26 +17,31 @@ namespace BrainVR.UnityFramework.InputControl
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetButtonDown("Point"))
-            {
-                if (ButtonPressed != null) ButtonPressed("Point");
-                if (PointButtonPressed != null) PointButtonPressed();
-            }
-
+            if (Input.GetButtonDown("Point")) Pointed();
             if (Input.GetButtonDown("Unstuck")) PlayerController.Instance.Unstuck();
-            if (Input.GetButtonDown("Confirm"))
-            {
-                if (ConfirmationButtonPressed != null) ConfirmationButtonPressed();
-            }
-            if (Input.GetButtonDown("Menu"))
-            {
-                if (MenuButtonPressed != null) MenuButtonPressed();
-            }
-            if (Input.GetButtonDown("Cancel"))
-            {
-                if (CancelButtonPressed != null) CancelButtonPressed();
-            }
-
+            if (Input.GetButtonDown("Confirm")) Confirmed();
+            if (Input.GetButtonDown("Menu")) MenuPressed();
+            if (Input.GetButtonDown("Cancel")) Canceled();
         }
+
+        #region EventRaisers
+        public static void Pointed()
+        {
+            if (ButtonPressed != null) ButtonPressed("Point");
+            if (PointButtonPressed != null) PointButtonPressed();
+        }
+        public static void Confirmed()
+        {
+            if (ConfirmationButtonPressed != null) ConfirmationButtonPressed();
+        }
+        public static void MenuPressed()
+        {
+            if (MenuButtonPressed != null) MenuButtonPressed();
+        }
+        public static void Canceled()
+        {
+            if (CancelButtonPressed != null) CancelButtonPressed();
+        }
+        #endregion
     }
 }
