cba9f12d77f3f00b6c1ef2cb0c96d76904375bfa
hejtmy <hejtmy@gmail.com>
2017-10-12 17:19:03 -0700

    Adds all demo files into the submodule

---
+++ Demo/Scripts/Demo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BrainVR.UnityFramework.Player;
+using UnityEngine;
+
+public class Demo : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.C))
+	    {
+	        var controller = (RigidBodyPlayerController) PlayerController.Instance;
+	        controller.Stop();
+	    }
+	}
+}
