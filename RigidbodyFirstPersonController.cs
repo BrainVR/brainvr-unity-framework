d34b4ca2e5c53d3a8d75bdceb013a05968990ff9
hejtmy <hejtmy@gmail.com>
2017-11-15 22:37:00 -0800

    Fixes player and mves it to standard assets folder

--- RigidbodyFirstPersonController.cs
+++ RigidbodyFirstPersonController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 using UnityStandardAssets.CrossPlatformInput;
 
 namespace BrainVR.UnityFramework.StandardAssets
