4e6cbcb48bc1d795ef334227b974881861abc992
hejtmy <hejtmy@gmail.com>
2017-12-05 00:26:26 -0800

    Closes #40

--- Logger/Loggers/Log.cs
+++ Logger/Loggers/Log.cs
@@ -44,6 +44,7 @@ namespace BrainVR.UnityLogger
                 DateTime dateTime = DateTime.Now;
                 timeStamp = dateTime.ToString("HH-mm-ss-dd-MM-yyy");
             }
+            DateString = timeStamp;
             // path + id/day-month-year
             string folderName = id + "_" + DateTime.Now.ToString("dd-MM-yyy") + "/";
 
@@ -51,14 +52,14 @@ namespace BrainVR.UnityLogger
             //creates a folder for the patient, if it doesnt exist
             Directory.CreateDirectory(FilePath);
             //vytvoři logfile - poslední parametr určuje, zda v případě existejnce souboru bude pokračovat v zápisu (true), či jej smaže (false);
-            _logFile = new StreamWriter(FilePath + id + "_" + logName + "_" + timeStamp + ".txt", true) {AutoFlush = true};
+            _logFile = new StreamWriter(FilePath + id + "_" + logName + "_" + DateString + ".txt", true) {AutoFlush = true};
             //because we can restart etc. many times during a day, the file continues but always writes a new header
             WriteHeader(id);
         }
         //simple header file for each new star of the experiment
         private void WriteHeader(string id)
         {
-            Header header = new Header(id, DateTime.Now.ToString("HH-mm-ss-dd-MM-yyy"));
+            Header header = new Header(id, DateString);
             _logFile.WriteLine("***SESSION HEADER***");
             _logFile.WriteLine(JsonConvert.SerializeObject(header, Formatting.Indented));
             _logFile.WriteLine("---SESSION HEADER---");
