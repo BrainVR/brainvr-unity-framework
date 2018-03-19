b76b08fc5a8a287de0b863a755bcd1be3ab4732b
hejtmy <hejtmy@gmail.com>
2018-03-19 07:11:04 -0700

    Simplifies the fix

--- Navigation/Scripts/LineNavigation.cs
+++ Navigation/Scripts/LineNavigation.cs
@@ -7,7 +7,6 @@ namespace BrainVR.UnityFramework.Navigation
     public class LineNavigation : NavigationController
     {
         LineRenderer _lineRenderer;
-        private int _lastPositionCount = -1;
         #region MonoBehaviour
         void Start()
         {
@@ -49,8 +48,7 @@ namespace BrainVR.UnityFramework.Navigation
             var positionCount = path.corners.Length; //set the array of positions to the amount of corners
             _lineRenderer.SetPosition(0, PlayerController.Instance.transform.position);
             //we don't redraw this until its needed. - until position count doesn't change we are still looking only for the line to the first "node"
-            if (_lastPositionCount == positionCount) return;
-            _lastPositionCount = positionCount;
+            if (_lineRenderer.positionCount == positionCount) return;
             _lineRenderer.positionCount = positionCount;
             for (var i = 1; i < path.corners.Length; i++)
             {
