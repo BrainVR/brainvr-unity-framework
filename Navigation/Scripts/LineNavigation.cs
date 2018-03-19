a4087102a97978f3a9b7859a4930fedfdd48430d
hejtmy <hejtmy@gmail.com>
2018-03-19 07:10:11 -0700

    Adds optimisation of line redraw

--- Navigation/Scripts/LineNavigation.cs
+++ Navigation/Scripts/LineNavigation.cs
@@ -7,6 +7,7 @@ namespace BrainVR.UnityFramework.Navigation
     public class LineNavigation : NavigationController
     {
         LineRenderer _lineRenderer;
+        private int _lastPositionCount = -1;
         #region MonoBehaviour
         void Start()
         {
@@ -45,8 +46,12 @@ namespace BrainVR.UnityFramework.Navigation
         private void DrawLine(NavMeshPath path)
         {
             if (path.corners.Length <= 0) return;
-            _lineRenderer.positionCount = path.corners.Length; //set the array of positions to the amount of corners
+            var positionCount = path.corners.Length; //set the array of positions to the amount of corners
             _lineRenderer.SetPosition(0, PlayerController.Instance.transform.position);
+            //we don't redraw this until its needed. - until position count doesn't change we are still looking only for the line to the first "node"
+            if (_lastPositionCount == positionCount) return;
+            _lastPositionCount = positionCount;
+            _lineRenderer.positionCount = positionCount;
             for (var i = 1; i < path.corners.Length; i++)
             {
                 _lineRenderer.SetPosition(i, path.corners[i]); //go through each corner and set that to the line renderer's position
