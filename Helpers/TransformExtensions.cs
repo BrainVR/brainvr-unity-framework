77fbbe690f48b21d98193768dcf751d42fa37382
hejtmy <hejtmy@gmail.com>
2018-03-22 11:43:51 -0700

    Adds long time missing script

---
+++ Helpers/TransformExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TransformExtensions{
+
+    // Set the layer of this GameObject and all of its children.
+    public static void SetLayerRecursively(this Transform transform, int layer) {
+        transform.gameObject.layer = layer;
+        foreach (Transform t in transform)
+            t.SetLayerRecursively(layer);
+    }
+}
