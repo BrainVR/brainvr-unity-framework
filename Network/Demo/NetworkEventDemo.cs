a0c8acd2333c1124ed2c0698e993762f8ec9dc09
hejtmy <hejtmy@gmail.com>
2018-03-23 04:25:44 -0700

    Adds player position sending and event sending

---
+++ Network/Demo/NetworkEventDemo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BrainVR.UnityFramework.Networking
+{
+    public class NetworkEventDemo : MonoBehaviour
+    {
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F3)) SendTestInfo();
+            if (Input.GetKeyDown(KeyCode.F4)) NetworkRestController.Instance.StartSendingPlayerInformation();
+            if (Input.GetKeyDown(KeyCode.F5)) NetworkRestController.Instance.StopSendingPlayerInformation();
+        }
+
+        public void SendTestInfo()
+        { 
+            var test = new Dictionary<string, string> { { "key", "value" }, { "secondKey", "secondValue" } };
+            StartCoroutine(NetworkRestController.Instance.SendPost(test));
+        }
+
+        void OnTriggerEnter()
+        {
+            var test = new Dictionary<string, string> { { "event", "Player entered zone" } };
+            StartCoroutine(NetworkRestController.Instance.SendPost(test));
+        }
+
+        void OnTriggerExit()
+        {
+            var test = new Dictionary<string, string> { { "event", "Player left zone" } };
+            StartCoroutine(NetworkRestController.Instance.SendPost(test));
+        }
+    }
+}
---
+++ Network/NetworkRestController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using PlayerController = BrainVR.UnityFramework.Player.PlayerController;
+
+namespace BrainVR.UnityFramework.Networking
+{
+    public class NetworkRestController : Singleton<NetworkRestController>
+    {
+        [SerializeField]
+        private string _url = "http://localhost";
+
+        [SerializeField]
+        private int _port = 80;
+
+        private IEnumerator _playerSender;
+
+        public void StartSendingPlayerInformation()
+        {
+            _playerSender = ContinuousPlayerPost(0.25f);
+            StartCoroutine(_playerSender);
+        }
+
+        public void StopSendingPlayerInformation()
+        {
+            if(_playerSender != null) StopCoroutine(_playerSender);
+        }
+
+        public IEnumerator SendPost(Dictionary<string, string> dict)
+        {
+            var form = new WWWForm();
+            foreach (var di in dict)
+            {
+                form.AddField(di.Key, di.Value);
+            }
+            var address = _url + ":" + _port;
+            // Upload to a cgi script
+            var w = UnityWebRequest.Post(address, form);
+            yield return w.Send();
+
+            if (w.isNetworkError || w.isHttpError) Debug.Log(w.error);
+            else Debug.Log("Finished POST");
+        }
+
+        private IEnumerator ContinuousPlayerPost(float time)
+        {
+            while (true)
+            {
+                var playerInformation = PlayerController.Instance.PlayerInformation().ToArray();
+                var postContent = string.Join(";", playerInformation);
+                StartCoroutine(SendPost(new Dictionary<string, string> { { "player", postContent } }));
+                yield return new WaitForSeconds(time);
+            }
+        }
+    }
+}
