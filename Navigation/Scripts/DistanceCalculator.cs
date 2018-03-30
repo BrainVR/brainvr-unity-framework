1f6e97b71ed0b1c5e5c05186b145d73241409c9a
hejtmy <hejtmy@gmail.com>
2018-03-30 01:08:54 -0700

    Adds distance calculator

---
+++ Navigation/Scripts/DistanceCalculator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace BrainVR.UnityFramework.Navigation
+{
+    public class DistanceCalculator : MonoBehaviour
+    {
+        public NavMeshAgent Agent;
+        public Transform Goal;
+
+        public float CalculateDistance()
+        {
+            var path = new NavMeshPath();
+            NavMesh.CalculatePath(Agent.gameObject.transform.position, Goal.position, NavMesh.AllAreas, path);
+            Debug.Log(path);
+            return path.corners.Length;
+        }
+    }
+#if UNITY_EDITOR
+    [CustomEditor(typeof(DistanceCalculator), true)]
+    public class DistanceCalculatorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var myScript = (DistanceCalculator)target;
+            if(GUILayout.Button("calculate distance"))
+            {
+                Debug.Log(CalculatePath(myScript));
+            }
+        }
+
+        private static float CalculatePath(DistanceCalculator calc)
+        {
+            return calc.CalculateDistance();
+        }
+    }
+#endif
+}
