23455643cbc5de7dfdd506ab9762f95f498efe81
hejtmy <hejtmy@gmail.com>
2018-03-30 03:36:05 -0700

    Ads map sending

--- Network/Demo/NetworkEventDemo.cs
+++ Network/Demo/NetworkEventDemo.cs
@@ -6,6 +6,11 @@ namespace BrainVR.UnityFramework.Networking
     public class NetworkEventDemo : MonoBehaviour
     {
 
+        void Start()
+        {
+            NetworkRestController.Instance.SendCustomInfo("map", "size", Terrain.activeTerrain.terrainData.size.ToString("N1"));
+        }
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.F4)) NetworkRestController.Instance.StartSendingPlayerInformation();
