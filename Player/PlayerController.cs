3290a20bbbc0ad450d549e2803a89153fa732f88
hejtmy <hejtmy@gmail.com>
2018-09-17 13:53:29 +0200

    Updates player controller with enabling physics and setting of rotation

--- Player/PlayerController.cs
+++ Player/PlayerController.cs
@@ -9,6 +9,7 @@ namespace BrainVR.UnityFramework.Player
         #region Public API
         #region Moving
         public abstract void EnableMovement(bool bo = true);
+        public abstract void EnablePhysics(bool bo = true);
         public abstract Vector3 Position { get; set; }
         public Vector2 Vector2Position
         {
@@ -20,7 +21,7 @@ namespace BrainVR.UnityFramework.Player
         }
         public void MoveToPosition(Vector2 position)
         {
-            Vector3 movePosition = new Vector3(position.x, gameObject.transform.position.y, position.y);
+            var movePosition = new Vector3(position.x, gameObject.transform.position.y, position.y);
             Position = movePosition;
         }
         public void Unstuck()
@@ -30,7 +31,7 @@ namespace BrainVR.UnityFramework.Player
         }
         #endregion
         #region Rotating
-        public abstract Vector2 Rotation { get; }
+        public abstract Vector2 Rotation { get; set; }
         public abstract void EnableRotation(bool bo = true);
         public abstract void LookAtPosition(Vector2 point);
         public abstract void LookAtPosition(Vector3 point);
