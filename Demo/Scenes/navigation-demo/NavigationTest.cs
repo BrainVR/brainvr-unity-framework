174bf8c3183638c07170477df16f0e050ad449dd
hejtmy <hejtmy@gmail.com>
2018-10-18 20:07:38 +0200

    Adds navigation demo

---
+++ Demo/Scenes/navigation-demo/NavigationTest.cs
@@ -0,0 +1,23 @@
+﻿using BrainVR.UnityFramework.Navigation;
+using UnityEngine;
+
+public class NavigationTest : MonoBehaviour
+{
+    public GameObject[] GameObjects;
+
+    private NavigationManager _navigationManager;
+	// Use this for initialization
+	void Start ()
+	{
+	    _navigationManager = NavigationManager.Instance;
+	    _navigationManager.SetTargets(GameObjects);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("o")) _navigationManager.StartNavigation();
+        if (Input.GetKeyDown("m")) _navigationManager.SetNavigationMode("Arrow");
+        if (Input.GetKeyDown("l")) _navigationManager.SetNavigationMode("Line");
+	}
+}
