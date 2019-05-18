10ac0b6a85dfe03bf344c36362704c1d89f7dc1d
hejtmy <hejtmy@gmail.com>
2019-05-18 21:52:42 +0200

    Reworks input manager to interface

---
+++ Logger/Interfaces/IInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BrainVR.UnityFramework.Logger.Interfaces
+{
+    public class ButtonPressedArgs : EventArgs
+    {
+        public string ButtonName;
+    }
+    public interface IInput
+    {
+        event EventHandler<ButtonPressedArgs> ButtonPressed;
+    }
+}
--- Logger/Interfaces/InputManagerBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BrainVR.UnityFramework.Logger.Interfaces
-{
-    public abstract class InputManagerBase
-    {
-        public delegate void ButtonPressedHandler(string name);
-        public static event ButtonPressedHandler ButtonPressed;
-    }
-}
-
